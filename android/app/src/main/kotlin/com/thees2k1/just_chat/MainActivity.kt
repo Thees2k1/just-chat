655b3aafe1a944d9b5135ca5f329735b0f6cd472
Thees2k1 <tongnguyenthe@gmail.com>
2025-09-02 14:25:07 +0700

    Integrate Firebase authentication and core services into the app - Add Firebase configuration files and update build settings - Implement authentication screen with form validation - Update dependencies for Firebase packages - Register Firebase plugins for macOS and Windows

--- android/app/src/main/kotlin/com/thees2k1/just_chat/MainActivity.kt
+++ android/app/src/main/kotlin/com/thees2k1/just_chat/MainActivity.kt
@@ -1,6 +1,4 @@
-//package com.example.just_chat
 package com.thees2k1.just_chat
-
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -9,33 +9,107 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+    bool isLogin = true;
+    String email = '';
+    String password = '';
+    final formKey = GlobalKey<FormState>();
+
+  void submit(){
+    if(formKey.currentState!.validate()){
+      formKey.currentState!.save();
+      print('Email: $email, Password: $password, isLogin: $isLogin');
+    };
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primary,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              alignment: Alignment.center,
-              child: Image.asset(Assets.chatImage),
-            ),
-            SizedBox(height: 12,),
-            Card(
-              child: Form(child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(children: [
-                  TextFormField(keyboardType: TextInputType.emailAddress,initialValue: "example@gmail.com",),
-                  const SizedBox(height: 8,),
-                  TextFormField(
-                    keyboardType: TextInputType.text,
-                    obscureText: true,
-                  ),
-                ],),
-              )
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(24),
+                alignment: Alignment.center,
+                width: 240,
+                height: 240,
+                child: Image.asset(Assets.chatImage, fit: BoxFit.cover),
               ),
-            )
-          ],
+              SizedBox(height: 12),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Card(
+                  child: Form(
+                    key: formKey,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            decoration: InputDecoration(
+                              labelText: "Email Address: ",
+                              hintText: "Enter your email address"
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                            validator: (value){
+                              if( value == null || value.isEmpty || !value.contains('@')){
+                                return 'Please enter a valid email address';
+                              }
+                              return null;
+                            },
+                            onSaved: (value){
+                              email = value!;
+                            },
+                          ),
+                          const SizedBox(height: 8),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              labelText: "Password: ",
+                            ),
+                            keyboardType: TextInputType.text,
+                            obscureText: true,
+                            validator: (value) {
+                              if (value == null || value.isEmpty || value.length < 6) {
+                                return 'Please enter a valid password (min. 6 characters)';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              password = value!;
+                            },
+
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 12),
+              ElevatedButton(
+                onPressed: submit,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(
+                    context,
+                  ).colorScheme.primaryContainer,
+                ),
+                child: Text(isLogin == true ? "Login" : "Signup"),
+              ),
+              TextButton(
+                onPressed: () => setState(() {
+                  isLogin = !isLogin;
+                }),
+                style: TextButton.styleFrom(textStyle: TextStyle(
+                  color: Colors.white,
+                )),
+                child: Text(
+                  isLogin == true
+                      ? 'Create new account'
+                      : 'Already have an account',
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
